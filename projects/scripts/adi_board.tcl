bbac025e6bc9faecaab241201b6991cee859d89a
Lars-Peter Clausen <lars@metafoo.de>
2018-06-15 17:03:40 +0200

    adi_boad.tcl: ad_xcvrcon: Add option to specify device clock
    Sometimes the output clock of the transceiver should not be used for the
    device clock.
    
    E.g. for deterministic latency with no uncertainty the device clock needs
    to be sourced directly from a clock or transceiver reference clock input
    pin.
    
    Add an option to the ad_xcvrcon command to specify the device clock.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- projects/scripts/adi_board.tcl
+++ projects/scripts/adi_board.tcl
@@ -174,7 +174,7 @@ proc ad_reconct {p_name_1 p_name_2} {
 # lane_map maps the logical lane $n onto the physical lane $lane_map[$n]. If no
 # lane map is provided logical lane $n is mapped onto physical lane $n.
 #
-proc ad_xcvrcon {u_xcvr a_xcvr a_jesd {lane_map {}}} {
+proc ad_xcvrcon {u_xcvr a_xcvr a_jesd {lane_map {}} {device_clk {}}} {
   
   global xcvr_index
   global xcvr_tx_index
@@ -238,6 +238,10 @@ proc ad_xcvrcon {u_xcvr a_xcvr a_jesd {lane_map {}}} {
   create_bd_port -dir ${ctrl_dir} $m_sync
   ad_ip_instance proc_sys_reset ${a_jesd}_rstgen
 
+  if {$device_clk == {}} {
+    set device_clk ${u_xcvr}/${txrx}_out_clk_${index}
+  }
+
   for {set n 0} {$n < $no_of_lanes} {incr n} {
 
     set m [expr ($n + $index)]
@@ -262,7 +266,7 @@ proc ad_xcvrcon {u_xcvr a_xcvr a_jesd {lane_map {}}} {
     }
 
     ad_connect  ${a_xcvr}/up_ch_${n} ${u_xcvr}/up_${txrx}_${m}
-    ad_connect  ${u_xcvr}/${txrx}_out_clk_${index} ${u_xcvr}/${txrx}_clk_${m}
+    ad_connect  ${device_clk} ${u_xcvr}/${txrx}_clk_${m}
     if {$jesd204_type == 0} {
       ad_connect  ${u_xcvr}/${txrx}_${phys_lane} ${a_jesd}/${txrx}_phy${n}
     } else {
@@ -278,19 +282,19 @@ proc ad_xcvrcon {u_xcvr a_xcvr a_jesd {lane_map {}}} {
   if {$jesd204_type == 0} {
     ad_connect  ${a_jesd}/sysref $m_sysref
     ad_connect  ${a_jesd}/sync $m_sync
-    ad_connect  ${u_xcvr}/${txrx}_out_clk_${index} ${a_jesd}/device_clk
+    ad_connect  ${device_clk} ${a_jesd}/device_clk
 #    if {$tx_or_rx_n == 0} {
 #      ad_connect  ${a_xcvr}/up_status ${a_jesd}/phy_ready
 #    }
   } else {
     ad_connect  ${a_jesd}/${txrx}_sysref $m_sysref
     ad_connect  ${a_jesd}/${txrx}_sync $m_sync
-    ad_connect  ${u_xcvr}/${txrx}_out_clk_${index} ${a_jesd}/${txrx}_core_clk
+    ad_connect  ${device_clk} ${a_jesd}/${txrx}_core_clk
     ad_connect  ${a_xcvr}/up_status ${a_jesd}/${txrx}_reset_done
     ad_connect  ${a_jesd}_rstgen/peripheral_reset ${a_jesd}/${txrx}_reset
   }
 
-  ad_connect  ${u_xcvr}/${txrx}_out_clk_${index} ${a_jesd}_rstgen/slowest_sync_clk
+  ad_connect  ${device_clk} ${a_jesd}_rstgen/slowest_sync_clk
   ad_connect  sys_cpu_resetn ${a_jesd}_rstgen/ext_reset_in
 
   if {$tx_or_rx_n == 0} {
